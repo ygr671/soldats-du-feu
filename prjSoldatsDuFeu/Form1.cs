8dd182b2df61dda2c2a4c5d4fba444e5afa516ee
Muhammed ERDAL <m.erdal.pro@proton.me>
2025-05-07 10:19:04 +0200

    Ajout de la procédure FillDataSet() dans frmTableauDeBord

--- prjSoldatsDuFeu/Form1.cs
+++ prjSoldatsDuFeu/Form1.cs
@@ -15,10 +15,45 @@ namespace prjSoldatsDuFeu
     {
         SQLiteConnection cx = Connexion.Connec;
 
+        public void FillDataSet(SQLiteConnection cx, DataSet ds)
+        {
+            try
+            {
+                string req;
+                DataTable schemaTable = cx.GetSchema("Tables");
+                string liste = "";
+
+                for (int i = 0; i < schemaTable.Rows.Count; i++)
+                {
+                    string nomTable = schemaTable.Rows[i][2].ToString();
+                    req = "SELECT * FROM " + nomTable;
+
+                    using (SQLiteCommand cd = new SQLiteCommand(req, cx))
+                    {
+                        using (SQLiteDataAdapter da = new SQLiteDataAdapter(cd))
+                        {
+                            da.Fill(ds, nomTable);
+                        }
+                    }
+
+                    liste += nomTable + "\n";
+                }
+
+                MessageBox.Show(liste + "\nNombre de tables chargées : " + ds.Tables.Count.ToString());
+            }
+            catch (SQLiteException err)
+            {
+                MessageBox.Show("Erreur SQLite : " + err.Message);
+            }
+        }
+
+
         public frmTableauDeBord()
         {
             InitializeComponent();
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
